a8d61a998f0630aedb4240871be75e77559d7791
chenshiliang <352056038@qq.com>
2018-11-18 23:09:06 +0800

    h5图片压缩上传

---
+++ js/index.js
@@ -0,0 +1,82 @@
+var vm = new Vue({
+    el: '#jddk',
+    data: {
+        imgs:[],
+        addShow:true,
+        mltoast:{
+            show:false,
+            text:''
+        }
+    },
+    mounted:function(){
+
+    },
+    methods:{
+        // 选择图片
+        chooseImg:function(e){
+            var that = this;
+            var files = e.target.files;
+            var imgsLength = this.imgs.length;
+            var maxLength = 3;
+             //还可以上传的图片数量
+            var count = maxLength - imgsLength;
+            // 限制最多3张
+            if (imgsLength >= maxLength) {
+                this.addShow = false;
+                this.toast('最多上传3张');
+            }
+            if(files.length >= 3){
+                this.addShow = false;
+                this.toast('最多上传3张');
+            }
+            // 有选择图片
+            if (count != 0) {
+                for (let i = 0; i < count; i++) {
+                    canvasResize(files[i], {
+                        crop: false,
+                        quality: 0.9,
+                        rotate: 0,
+                        callback(baseStr) {
+                            that.imgs.push(baseStr)
+                        }
+                    })
+                }  
+            }
+
+             
+        },
+        // 删除图片
+        delImg:function(index){
+            console.log('dddd')
+            this.imgs.splice(index,1);
+            this.addShow = true;
+        },
+        // 上传到后台
+        submit:function(){
+            var that = this;
+            axios({
+                url: '',
+                method:'post',
+                data: {
+                    files: that.imgs
+                },
+                header: {"Content-Type": "multipart/form-data"}
+            })
+            .then(function(res) {
+                console.log(res)
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        },
+        // toast
+        toast:function(text){
+            this.mltoast.text = text;
+            this.mltoast.show = true;
+            setTimeout(() => {
+              this.mltoast.show = false;
+            }, 1500)
+        }
+      
+    }
+})
